f13d74ef078fa1278ac916a051b2eca93554bd43
fredybel <fredybeltran712@gmail.com>
2024-11-23 15:11:27 -0600

    cambiando vista

--- FormulaCuadraticaFred/App.xaml.cs
+++ FormulaCuadraticaFred/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace FormulaCuadraticaFred
+﻿using FormulaCuadraticaFred.Views;
+
+namespace FormulaCuadraticaFred
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new FormulaCuadraticaView();
         }
     }
 }
